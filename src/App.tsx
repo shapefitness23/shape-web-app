df0bb2eb2de7b5391050840943aab18f31ac4dbd
Maharjun M <maharjun@rudderstack.com>
2023-10-01 19:23:28 +0530

    adding routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import './App.css';
+import { Routes, Route, Navigate } from "react-router-dom"
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { Authentication } from './components/auth';
+import './App.css';
 
 function App() {
   return (
     <GoogleOAuthProvider clientId="1076068628081-0odjmsav2v6qkkhnuvde8ns853o3tpq6.apps.googleusercontent.com">
-      <Authentication />
+      <Routes>
+        <Route path="/" element={<Authentication />} />
+        <Route path="*" element={<Navigate to="/"/>} />
+      </Routes>
     </GoogleOAuthProvider>
   );
 }
